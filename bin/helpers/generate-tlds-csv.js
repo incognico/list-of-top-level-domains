16159bb6cb37d0ef426a442aa27d112dd273a545
Doug Bird <doug@katmore.com>
2017-12-13 12:29:47 -0800

    refactor / simplify

---
+++ bin/helpers/generate-tlds-csv.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log("downloads the 'http://data.iana.org/TLD/tlds-alpha-by-domain.txt' file and cross references it with https://www.iana.org/domains/root/db to re-generate the 'tlds.csv' csv file");
+console.log("---this script is currently a 'to-do' placeholder!---");
+console.error("terminating... the development for the 'JSON' format generator has not yet been completed");
