2fb8666e0e87f3910975141b2e4ea34315458c4b
Doug Bird <doug@katmore.com>
2017-12-13 12:01:28 -0800

    npm package and path refactoring
    moved around paths to facilitate node project development and npm package

--- bin/generate-csv-tld-enum.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-console.log("generates the 'tlds-name-only.csv' CSV text file from the 'tlds.csv' csv file");
-console.log("---this script is currently a 'to-do' placeholder!---");
-console.error("terminating... the development for the 'CSV' (text) format generator has not yet been completed");
